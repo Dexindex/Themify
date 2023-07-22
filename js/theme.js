8c467d098ffc819ce23eb341d0e46f7e2ecbf85c
unknown <oussama2003berrad@gmail.com>
2023-07-22 19:00:57 +0100

    Themify

---
+++ js/theme.js
@@ -0,0 +1,43 @@
+function blue() {
+  theme.innerHTML =
+    "<style>:root {--bg: #0d6efd !important;--acc:#084298 !important;--acc2:#031633 !important;--color: white !important;}</style>";
+}
+
+function indigo() {
+  theme.innerHTML = "";
+}
+
+function red() {
+  theme.innerHTML =
+    "<style>:root {--bg: #dc3545!important;--acc:#842029 !important;--acc2:#dc3545 !important;--color: white !important;}</style>";
+}
+
+function orange() {
+  theme.innerHTML =
+    "<style>:root {--bg: #fd7e14 !important;--acc:#984c0c !important;--acc2:#fd9843 !important;--color: white !important;}</style>";
+}
+
+function pink() {
+  theme.innerHTML =
+    "<style>:root {--bg: #d63384 !important;--acc:#561435 !important;--acc2:#e685b5 !important;--color: white !important;}</style>";
+}
+
+function yellow() {
+  theme.innerHTML =
+    "<style>:root {--bg: #ffc107 !important;--acc:#997404 !important;--acc2:#ffda6a !important;--color: white !important;}</style>";
+}
+
+function green() {
+  theme.innerHTML =
+    "<style>:root {--bg: #198754 !important;--acc:#0a3622 !important;--acc2:#051b11 !important;--color: white !important;}</style>";
+}
+
+function teal() {
+  theme.innerHTML =
+    "<style>:root {--bg: #20c997 !important;--acc:#13795b !important;--acc2:#06281e !important;--color: white !important;}</style>";
+}
+
+function black() {
+  theme.innerHTML =
+    "<style>:root {--bg: #222 !important;--acc:#111 !important;--acc2:#000 !important;--color: white !important;}</style>";
+}
